6a63d047716bd62b97dc63db65c53792a456c436
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-12-01 19:28:49 +0900

    次の問題に進めるようにしよう

--- js/main.js
+++ js/main.js
@@ -32,12 +32,18 @@
     } else {
       li.classList.add('wrong');
     }
+
+    btn.classList.remove('disabled');
   }
 
   function setQuiz() {
     isAnswered = false;
 
     question.textContent = quizSet[currentNum].q;
+
+    while (choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
 
     const shuffledChoices = shuffle([...quizSet[currentNum].c]);
     shuffledChoices.forEach(choice => {
@@ -51,4 +57,9 @@
   }
 
   setQuiz();
+
+  btn.addEventListener('click', () => {
+    currentNum++;
+    setQuiz();
+  });
 }
